21e093d62c7c8f1498fe2edeb0e4fb2bc981ebe7
StefanDespotovic <stefadesa@gmail.com>
2023-03-14 17:21:11 +0100

    React-weather-app

---
+++ src/components/Weather/Weather.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+
+const Weather = ({ city }) => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=65db83cf65e337e4782db3074f7add50&units=metric`
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setWeatherData(data);
+          setErrorMessage(null);
+        } else {
+          setErrorMessage("City not found. Please try again.");
+        }
+      } catch (error) {
+        setErrorMessage("Error fetching weather data. Please try again later.");
+      }
+    };
+
+    fetchWeatherData();
+  }, [city]);
+
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  if (errorMessage) {
+    return (
+      <div>
+        <p>{errorMessage}</p>
+        <button onClick={handleRefresh}>Refresh</button>
+      </div>
+    );
+  }
+
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>
+        {weatherData.name}, {weatherData.sys.country}
+      </h2>
+      <p>Current temperature: {weatherData.main.temp}°C</p>
+      <p>Feels like: {weatherData.main.feels_like}°C</p>
+      <p>Weather: {weatherData.weather[0].description}</p>
+    </div>
+  );
+};
+
+export default Weather;
---
+++ src/components/weatherSearch/WeatherSearch.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Weather from "../Weather/Weather";
+
+const WeatherSearch = () => {
+  const [city, setCity] = useState("");
+  const [showWeather, setShowWeather] = useState(false);
+  const [showRefreshButton, setShowRefreshButton] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=65db83cf65e337e4782db3074f7add50&units=metric`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("City not found");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setShowWeather(true);
+        setShowRefreshButton(true);
+      })
+      .catch((error) => {
+        setShowError(true);
+      });
+  };
+
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  const handleErrorMessageClose = () => {
+    setShowError(false);
+    handleRefresh();
+  };
+
+  return (
+    <div>
+      {!showWeather && (
+        <form onSubmit={handleSearch}>
+          <label>
+            Enter a city name:
+            <input
+              type="text"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </label>
+          <button type="submit">Search</button>
+        </form>
+      )}
+      {showWeather && <Weather city={city} />}
+      {showRefreshButton && <button onClick={handleRefresh}>Refresh</button>}
+      {showError && (
+        <div>
+          <p>City not found. Please try again.</p>
+          <button onClick={handleErrorMessageClose}>Try again</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherSearch;
